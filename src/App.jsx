7640495e3576ef54235114dc4e1c739e82ea2de1
takashioguchi <takashi.o0321@gmail.com>
2025-12-16 17:07:07 +0900

    feat: add additional Test component with id 3

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <Test id="1" name="takashi" tel="110" />
       <Test id="2" name="hanako" tel="119" />
+      <Test id="3" name="taro" tel="9110" />
     </>
   );
 }

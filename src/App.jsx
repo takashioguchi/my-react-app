dd1fb544a59bdcfd2fab4c6f96fc0c0eb8d1c129
takashioguchi <takashi.o0321@gmail.com>
2025-12-17 15:16:23 +0900

    feat: refactor App component to use native buttons and update Btn component styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import { useState } from "react";
-import Btn from "./Btn.jsx";
+//import Btn from "./Btn.jsx";
 import "./styles/animation.css";
 import "./Test.jsx";
 
 function App() {
+  const [onoff, setOnOff] = useState(false);
   const [count, setCount] = useState(0);
+
+  const handleChange = (e) => {
+    setOnOff(e.target.checked);
+  };
+
   const handleClick = () => {
     setCount(count + 1);
   };
   const handleTap = () => {
     setCount(count - 1);
   };
+
   return (
     <>
-      <Btn btnClick={handleClick}>click</Btn>
-      <Btn btnClick={handleTap}>tap</Btn>
+      <button onClick={handleClick} disabled={!onoff}>
+        countup
+      </button>
+      <button onClick={handleTap} disabled={!onoff}>
+        countdown
+      </button>
       <h1>{count}</h1>
+      <label htmlFor="kidou">
+        <input
+          id="kidou"
+          type="checkbox"
+          checked={onoff}
+          onChange={handleChange}
+        />
+        起動
+      </label>
     </>
   );
 }
--- src/Btn.jsx
+++ src/Btn.jsx
@@ -2,7 +2,11 @@ const Btn = ({ btnClick, children }) => {
   return (
     <button
       onClick={btnClick}
-      className="p-4 m-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition-colors"
+      disabled={false}
+      className="p-4 m-2 bg-blue-500 text-white rounded-lg
+  hover:bg-blue-700
+  disabled:opacity-50
+  disabled:cursor-not-allowed"
     >
       {children}
     </button>
